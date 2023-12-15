7511874a7265dd3dd92682562e08dfcd472e2c52
Juan-Diego Bermeo-Ortiz <jbermeo@biwidl319.ee.ethz.ch>
2023-12-15 11:21:35 +0100

    added bash scripts and basic remapping of labels for ADAM dataset

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
+# Minimum to run nnUNet
 conda install pytorch==2.1.0 torchvision==0.16.0 pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install nnunetv2
 pip install --upgrade git+https://github.com/FabianIsensee/hiddenlayer.git
 conda install graphviz
 pip install IPython
+
+# Extra stuff
+pip install jupyter
