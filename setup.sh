6fa18cb9fe10562cfc15d1206f92102fd1cc3d2a
Juan Bermeo <juandiego.bermeoortiz@uzh.ch>
2023-12-18 16:56:21 +0100

    added small notebook to measure performance of the tasks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 # Minimum to run nnUNet
 conda install pytorch==2.1.0 torchvision==0.16.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+
+# Install nnUnet
 pip install nnunetv2
+#git clone https://github.com/MIC-DKFZ/nnUNet.git
+#cd nnUNet
+#pip install -e .
 pip install --upgrade git+https://github.com/FabianIsensee/hiddenlayer.git
 conda install graphviz
 pip install IPython
