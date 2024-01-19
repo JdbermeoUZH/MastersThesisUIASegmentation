b53b430de1559ec419546831c9a52019adcd1546
Juan-Diego Bermeo-Ortiz <jbermeo@biwidl319.ee.ethz.ch>
2024-01-19 17:50:06 +0100

    corrected small bugs

--- scripts/training/segmentation/train_2D_segmentation_model.sh
+++ scripts/training/segmentation/train_2D_segmentation_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --output=../../logs/%j_train_dae_hcp.out
+#SBATCH --output=../../logs/%j_train_segmentation_2D.out
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks=2
 #SBATCH --gres=gpu:1
@@ -8,7 +8,7 @@ cd ../tta_uia_segmentation/src/preprocessing
 source /itet-stor/jbermeo/net_scratch/conda/etc/profile.d/conda.sh
 conda activate nnUnet_dev
 
-python train_dae_hcp.py \
+python train_2D_segmentation_model.py \
  /scratch_net/biwidl319/jbermeo/MastersThesisUIASegmentation/config/datasets.yaml \
  /scratch_net/biwidl319/jbermeo/MastersThesisUIASegmentation/config/models.yaml \
  /scratch_net/biwidl319/jbermeo/MastersThesisUIASegmentation/config/training_hcp_t1w.yaml\
