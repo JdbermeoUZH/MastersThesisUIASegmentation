fixed error in definition of normalization module for 2D segmentation

scripts/tta/dae_tta.py
@@ -1,17 +1,22 @@
-import argparse
-import copy
-import numpy as np
 import os
+import sys
+import copy
+import argparse
+
+import wandb
 import torch
 import torch.nn.functional as F
 from torch.optim import lr_scheduler
 from torch.utils.data import Subset, DataLoader, ConcatDataset, TensorDataset
-import wandb
+import numpy as np
+
+sys.path.append(os.path.normpath(os.path.join(
+    os.path.dirname(__file__), '..', '..', '..', 'tta_uia_segmentation', 'src')))
 
 from dataset.dataset import get_datasets
 from models import Normalization, UNet
 from models.normalization import background_suppression
-from tta. import test_volume
+from tta.test import test_volume
 from utils.io import load_config, dump_config, print_config, save_checkpoint, write_to_csv, deep_get
 from utils.utils import seed_everything, get_seed
 from utils.loss import DiceLoss, class_to_onehot, dice_score, onehot_to_class

tta_uia_segmentation/src/models/normalization.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+import models.utils as mu
 from utils.io import deep_get
 from utils.utils import assert_in
 
@@ -63,7 +64,17 @@ class Normalization(nn.Module):
     
     
     """
-    def __init__(self, n_layers=3, image_channels=1, channel_size=16, kernel_size=3, activation=None, batch_norm=True, residual=True):
+    def __init__(
+        self,
+        n_layers: int = 3,
+        image_channels: int = 1,
+        channel_size: int = 16,
+        kernel_size: int = 3, 
+        activation=None,
+        batch_norm: bool = True,
+        residual: bool = True,
+        n_dimensions: int = 2
+        ):
         super().__init__()
 
         assert n_layers > 0, "Normalization network has no layers"
@@ -74,10 +85,11 @@ class Normalization(nn.Module):
 
         layers = []
         for in_size, out_size in zip(channel_sizes, channel_sizes[1:]):
-            layers += [nn.Conv3d(in_size, out_size, kernel_size,
-                                 padding='same', padding_mode='reflect')]
+            layers += [mu.get_conv(in_size, out_size, kernel_size, 
+                                   padding='same', padding_mode='reflect',
+                                   n_dimensions=n_dimensions)]
             if batch_norm:
-                layers += [nn.BatchNorm3d(out_size)]
+                layers += [mu.get_batch_norm(out_size, n_dimensions=n_dimensions)]
 
             if activation == 'relu':
                 layers += [nn.ReLU()]
@@ -86,8 +98,9 @@ class Normalization(nn.Module):
             elif activation == 'rbf':
                 layers += [RBF(out_size)]
 
-        layers += [nn.Conv3d(channel_sizes[-1], image_channels,
-                             kernel_size, padding='same', padding_mode='reflect')]
+        layers += [mu.get_conv(channel_sizes[-1], image_channels, 
+                               kernel_size, padding='same', padding_mode='reflect',
+                               n_dimensions=n_dimensions)]
 
         self.layers = nn.Sequential(*layers)
 

tta_uia_segmentation/src/train/DAETrainer.py
@@ -58,7 +58,7 @@ class DAETrainer:
             self._load_checkpoint(os.path.join(logdir, checkpoint_last))
         else:
             print('Starting training from scratch.')
-            self.best_validation_loss
+            self.best_validation_loss = np.inf
             self._save_checkpoint(os.path.join(logdir, checkpoint_last))
         
         self.loss_func = loss_func