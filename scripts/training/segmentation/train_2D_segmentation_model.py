fixed bugs and added config for wmh\n - added config wmh\n - added normalization for the ddpm dataset class

scripts/training/segmentation/train_2D_segmentation_model.py
@@ -2,23 +2,22 @@ import os
 import sys
 import argparse
 
-import torch
 import wandb
 import numpy as np
 from torch.utils.data import DataLoader
 
 sys.path.append(os.path.normpath(os.path.join(
-    os.path.dirname(__file__), '..', '..', '..', 'tta_uia_segmentation', 'src')))
+    os.path.dirname(__file__), '..', '..', '..')))
 
-from dataset.dataset_in_memory import get_datasets
-from models import Normalization, UNet
-from models.normalization import background_suppression
-from train import NormSegTrainer
-from utils.loss import DiceLoss, dice_score
-from utils.io import (load_config, dump_config, print_config,
-                      save_checkpoint, write_to_csv, deep_get, rewrite_config_arguments)
-from utils.utils import seed_everything, define_device
-from utils.logging import setup_wandb
+from tta_uia_segmentation.src.dataset.dataset_in_memory import get_datasets
+from tta_uia_segmentation.src.models import Normalization, UNet
+from tta_uia_segmentation.src.train import NormSegTrainer
+from tta_uia_segmentation.src.utils.loss import DiceLoss
+from tta_uia_segmentation.src.utils.io import (
+    load_config, dump_config, print_config, 
+    write_to_csv, rewrite_config_arguments)
+from tta_uia_segmentation.src.utils.utils import seed_everything, define_device
+from tta_uia_segmentation.src.utils.logging import setup_wandb
 
 
 

tta_uia_segmentation/src/dataset/dataset_in_memory_for_ddpm.py
@@ -5,7 +5,7 @@ import h5py
 import torch
 import numpy as np
 from typing import Union
-from torch.utils.data import Subset, Dataset
+from torch.utils.data import Subset, Dataset, DataLoader
 from torchmetrics.functional.classification import dice
 
 from tta_uia_segmentation.src.dataset.dataset_in_memory import DatasetInMemory
@@ -53,7 +53,6 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
     def __init__(
         self,
         norm: Optional[torch.nn.Module],
-        paths_normalized_h5: dict[str, str],
         use_original_imgs: bool = False,
         concatenate_along_channel: bool = False,
         normalize: str = 'min_max',
@@ -61,11 +60,15 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
         intensity_value_range: Optional[tuple[float, float]] = None,
         norm_device: str = 'cpu',
         norm_neg_one_to_one: bool = False,
+        paths_normalized_h5: Optional[dict[str, str]] = None,
         *args,
         **kwargs,
     ):
         
         if norm is None and not use_original_imgs:
+            if paths_normalized_h5 is None:
+                raise ValueError('Either a normalization model or paths to normalized images should be given,'
+                                 'if not using original images')
             kwargs['paths'] = paths_normalized_h5
         
         super().__init__(*args, **kwargs)
@@ -77,7 +80,7 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
             self.norm.requires_grad_(False)
             self.norm.to(norm_device)
         
-        self.normalized_img_path = paths_normalized_h5[self.split]
+        self.normalized_img_path = paths_normalized_h5[self.split] if paths_normalized_h5 is not None else None
         
         self.concatenate_along_channel = concatenate_along_channel
         self.normalize = normalize
@@ -92,6 +95,8 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
         else:
             self.images_min, self.images_max = self._find_min_max_in_normalized_imgs()
         
+        print(f'Min and max values of normalized images: {self.images_min}, {self.images_max}')
+
     def __getitem__(self, index) -> Union[torch.Tensor, tuple[torch.Tensor, torch.Tensor]]:
 
         images = self.images[index, ...]
@@ -112,13 +117,20 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
             
         images = torch.from_numpy(images).float()
         labels = torch.from_numpy(labels).float()
-    
+        
         # Use the normalization model to normalize the images, if one is given
         if self.norm is not None:
             with torch.inference_mode():
                 images = images.to(self.norm_device)
                 labels = labels.to(self.norm_device)
                 images = self.norm(images[None, ...]).squeeze(0)
+                
+        # Update the min and max values of the images, in case a larger value is observed
+        img_max, img_min = images.max(), images.min()   
+        if img_min < self.images_min:
+            self.images_min = img_min
+        if img_max > self.images_max:
+            self.images_max = img_max
 
         # Normalize image and label map to [0, 1]
         if self.normalize == 'min_max':
@@ -163,10 +175,30 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
         return Subset(self, sampled_slices)
     
     def _find_min_max_in_normalized_imgs(self):
-        with h5py.File(self.normalized_img_path, 'r') as data:
-            images = data['images'][:]
-            images_min = images.min()
-            images_max = images.max()
+        if self.normalized_img_path is not None:    
+            with h5py.File(self.normalized_img_path, 'r') as data:
+                images = data['images'][:]
+                images_min = images.min()
+                images_max = images.max()
+        
+        elif self.norm is not None:
+            print('Determining min and max values of normalized images from a sample of the dataset')
+            self.images_min, self.images_max = np.inf, -np.inf
+            sample_dataset = self.sample_slices(256)
+            sample_dataset = DataLoader(sample_dataset, batch_size=4, num_workers=2)
+            
+            for img, _ in sample_dataset:
+                img = img.to(self.norm_device)
+                img = self.norm(img)
+                current_max, current_min = img.max(), img.min()
+                if current_min < self.images_min:
+                    images_min = current_min
+                if current_max > self.images_max:
+                    images_max = current_max
+                
+        else: 
+            raise ValueError('No normalization model or normalized images were given, '
+                             'thus the min and max values of normalized images cannot be determined')
             
         return images_min, images_max 
 

tta_uia_segmentation/src/train/OAICDDPMTrainer.py
@@ -11,6 +11,7 @@ import wandb
 import numpy as np
 import torch as th
 from PIL import Image   
+import torch.distributed as dist
 from torchvision.utils import make_grid, save_image 
 from torch.utils.data import DataLoader, Dataset
 from torchmetrics.functional.image import (
@@ -116,18 +117,23 @@ class OAICDDPMTrainer(TrainLoop):
             if self.step % self.log_interval == 0:
                 logged_info = logger.dumpkvs()
                 
-                if self.wandb_log:
-                    wandb.log(dict(logged_info))
+                if self.wandb_log and dist.get_rank() == 0:
+                    del logged_info['step']
+                    wandb.log(dict(logged_info),
+                              step=self.step + self.resume_step)
                 
             if self.step % self.save_interval == 0:
                 self.save()
                 
                 # Sample images and log their metrics to wandb
-                if self.measure_performance_on_train:
-                    self.measure_performance(self.train_data, 'train', self.num_samples_for_metrics)
-                
-                if self.measure_performance_on_val and self.val_data is not None:
-                    self.measure_performance(self.val_data, 'val', self.num_samples_for_metrics)
+                if self.wandb_log and dist.get_rank() == 0:
+                    if self.measure_performance_on_train:
+                        self.measure_performance(
+                            self.train_data, 'train', self.num_samples_for_metrics)
+                    
+                    if self.measure_performance_on_val and self.val_data is not None:
+                        self.measure_performance(
+                            self.val_data, 'val', self.num_samples_for_metrics)
             
             self.step += 1
             
@@ -167,12 +173,11 @@ class OAICDDPMTrainer(TrainLoop):
                 model_kwargs={"x_cond": x_cond}
             )
             
-            img = unnormalize_to_zero_to_one(img)
-            x_gen = unnormalize_to_zero_to_one(x_gen)
+            img = unnormalize_to_zero_to_one(img).cpu()
+            x_gen = unnormalize_to_zero_to_one(x_gen).cpu()
             n_classes = x_cond.shape[1]
             x_cond = onehot_to_class(unnormalize_to_zero_to_one(
-                x_cond))
-            x_cond = x_cond / (n_classes - 1)
+                x_cond)).cpu() / (n_classes - 1)
             
             for metric_name, metric_fn in self.metrics_to_log.items():
                 metric = metric_fn(x_gen, img).item()
@@ -183,21 +188,22 @@ class OAICDDPMTrainer(TrainLoop):
                     th.cat([x_cond, img, x_gen], dim = -1))
             
         all_images = th.cat(imgs_for_plot, dim = 0)
-        milestone = self.step // self.save_interval
-        all_images_fn = f'{dataset_name}-sample-m{milestone}-step-{self.step}-img_gt_seg_gt_gen_img.png'
+        global_step = self.step + self.resume_step
+        milestone = global_step // self.save_interval
+        all_images_fn = f'{dataset_name}-sample-m{milestone}-step-{global_step}-img_gt_seg_gt_gen_img.png'
         plot_rows = int(math.sqrt(len(imgs_for_plot)))
         save_image(all_images, os.path.join(get_blob_logdir(), all_images_fn), 
                    nrow = plot_rows)
 
         for metric_name, metric_values in metrics.items():
             wandb.log({f'{dataset_name}_{metric_name}': np.mean(metric_values)},
-                      step=self.step)
+                      step=global_step)
         
         # Log images in wandb
         wandb.log({
                 all_images_fn: wandb.Image(tensor_collection_to_image_grid(
                     all_images,nrow = plot_rows))}, 
-                step = self.step
+                step = global_step
             )
     
     def forward_backward(self, batch, cond):