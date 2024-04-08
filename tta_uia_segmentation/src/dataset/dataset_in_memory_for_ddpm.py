modified script to compare L_DDPM between source and target domains, fixed bug in backpropr through x_cond for L_DDPM

tta_uia_segmentation/src/dataset/dataset_in_memory_for_ddpm.py
@@ -6,12 +6,12 @@ import torch
 import numpy as np
 from typing import Union
 from torch.utils.data import Subset, Dataset, DataLoader
-from torchmetrics.functional.classification import dice
 
 from tta_uia_segmentation.src.dataset.dataset_in_memory import DatasetInMemory
 from tta_uia_segmentation.src.dataset.augmentation import apply_data_augmentation
 import tta_uia_segmentation.src.dataset.utils as du
 from tta_uia_segmentation.src.utils.utils import get_seed
+from tta_uia_segmentation.src.utils.loss import dice_score
 
 
 
@@ -184,6 +184,7 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
     def _find_min_max_in_normalized_imgs(self):
         if self.normalized_img_path is not None:    
             with h5py.File(self.normalized_img_path, 'r') as data:
+                print('DELETE ME')
                 images = data['images'][:]
                 images_min = images.min()
                 images_max = images.max()
@@ -197,6 +198,7 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
             for img, _ in sample_dataset:
                 img = img.to(self.norm_device)
                 img = self.norm(img)
+                
                 current_max, current_min = img.max(), img.min()
                 if current_min < self.images_min:
                     images_min = current_min
@@ -272,13 +274,21 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
         idxs_sample_filtered = []
 
         _, seg_orig = self[self.vol_and_z_idx_to_idx(vol_idx, z_idx)]
-        seg_orig = (seg_orig * (self.n_classes - 1)).int()
-        for z_idx in idxs_sample:
-            img_idx = self.vol_and_z_idx_to_idx(vol_idx, z_idx)
+        seg_orig = seg_orig[None, ...]
+        if not self.one_hot_encode and seg_orig.max() < 1.0:
+            seg_orig = (seg_orig * (self.n_classes - 1)).int()
+        
+        for z in idxs_sample:
+            img_idx = self.vol_and_z_idx_to_idx(vol_idx, z)
             _, seg_i = self[img_idx]
-            seg_i = (seg_i * (self.n_classes - 1)).int()
+            seg_i = seg_i[None, ...]
             
-            if dice(seg_i, seg_orig, ignore_index=0).item() > max_dice_score_threshold:
+            if not self.one_hot_encode and seg_i.max() < 1.0:
+                seg_i = (seg_i * (self.n_classes - 1)).int()
+            
+            _, dice_fg = dice_score(seg_i, seg_orig, reduction='mean')
+            
+            if dice_fg > max_dice_score_threshold:
                 continue
             
             idxs_sample_filtered.append(img_idx)
@@ -300,20 +310,33 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
         high_z_lim = min(self.dim_proc[0] - 1, z_idx + int(max_dist_z_frac * self.dim_proc[0]))
         
         # Sample positions in the range [low_z_lim, high_z_lim] 
+        if high_z_lim - low_z_lim < n:
+            n = high_z_lim - low_z_lim
+            print(f'WARNING: Not enough slices to sample, sampling {n} slices instead')
+         
         idxs_sample = random.sample(range(low_z_lim, high_z_lim), n)
         
         # Exclude all indexes that have a dice score that is too low
         idxs_sample_filtered = []
 
         _, seg_orig = self[self.vol_and_z_idx_to_idx(vol_idx, z_idx)]
-        seg_orig = (seg_orig * (self.n_classes - 1)).int()
-        for z_idx in idxs_sample:
-            img_idx = self.vol_and_z_idx_to_idx(vol_idx, z_idx)
+        seg_orig = seg_orig[None, ...]
+        
+        if not self.one_hot_encode and seg_orig.max() < 1.0:
+            seg_orig = (seg_orig * (self.n_classes - 1)).int()
+        
+        for z in idxs_sample:
+            img_idx = self.vol_and_z_idx_to_idx(vol_idx, z)
             _, seg_i = self[img_idx]
-            seg_i = (seg_i * (self.n_classes - 1)).int()
+            seg_i = seg_i[None, ...]
             
-            if dice(seg_i, seg_orig) < min_dice_score_threshold:
-                print('Dice score too low to use sample: ', dice(seg_i, seg_orig, ignore_index=0))
+            if not self.one_hot_encode and seg_i.max() < 1.0:
+                seg_i = (seg_i * (self.n_classes - 1)).int()
+            
+            _, dice_fg = dice_score(seg_i, seg_orig, reduction='mean')
+            
+            if dice_fg < min_dice_score_threshold:
+                print('Dice score too low to use sample: ', dice_fg)
                 continue
             
             idxs_sample_filtered.append(img_idx)
@@ -328,6 +351,10 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
         max_dist_z_frac: float = 0.2,
         min_dice_score_threshold: float = 0.55,
     ) -> list[int]:
+        """
+        TODO
+            - Repeat sampling with other volumes if not enough slices are sampled
+        """
         # Define the range of z indexes to sample from
         low_z_lim = max(0, z_idx - int(max_dist_z_frac * self.dim_proc[0]))
         high_z_lim = min(self.dim_proc[0] - 1, z_idx + int(max_dist_z_frac * self.dim_proc[0]))
@@ -341,20 +368,27 @@ class DatasetInMemoryForDDPM(DatasetInMemory):
         n_slices_per_volume = {vol_idx: n_slices for vol_idx, n_slices in zip(vol_idxs_to_sample, n_slices_per_volume)}
         
         _, seg_orig = self[self.vol_and_z_idx_to_idx(vol_idx, z_idx)]
-        seg_orig = (seg_orig * (self.n_classes - 1)).int()
+        seg_orig = seg_orig[None, ...]
+        if not self.one_hot_encode and seg_orig.max() < 1.0: 
+            seg_orig = (seg_orig * (self.n_classes - 1)).int()
+        
         idxs_sample_filtered = []
         for vol_idx, n_slices in n_slices_per_volume.items():
             if n_slices == 0:
                 continue
             
             sampled = 0 
-            for z_idx in range(low_z_lim, high_z_lim + 1):
-                img_idx = self.vol_and_z_idx_to_idx(vol_idx, z_idx)
+            for z in range(low_z_lim, high_z_lim + 1):
+                print(f'z: {z}')
+                img_idx = self.vol_and_z_idx_to_idx(vol_idx, z)
                 _, seg_i = self[img_idx]
-                seg_i = (seg_i * (self.n_classes - 1)).int()
+                seg_i = seg_i[None, ...]
                 
-                if dice(seg_i, seg_orig, ignore_index=0) < min_dice_score_threshold:
-                    #print('Dice score too low to use sample: ', dice(seg_i, seg_orig, ignore_index=0))
+                if not self.one_hot_encode and seg_i.max() < 1.0:
+                    seg_i = (seg_i * (self.n_classes - 1)).int()
+                
+                _, dice_fg = dice_score(seg_i, seg_orig, reduction='mean')
+                if dice_fg < min_dice_score_threshold:
                     continue
                 
                 idxs_sample_filtered.append(img_idx)

tta_uia_segmentation/src/models/ConditionalGaussianDiffusion.py
@@ -70,7 +70,7 @@ class ConditionalGaussianDiffusion(GaussianDiffusion):
         max_t = default(max_t, self.num_timesteps)
         t = torch.randint(min_t, max_t, (b,), device=device).long()
         img = self.normalize(img)
-        cond_img = self.normalize(cond_img.type(torch.int8))  
+        cond_img = self.normalize(cond_img)  
         
         if img.max() > 1 or img.min() < -1:
             print('Warning: img is not normalized between -1 and 1'

tta_uia_segmentation/src/models/io.py
@@ -72,7 +72,7 @@ def load_cddpm_from_configs_and_cpt(
     cpt_fp: str,
     img_size: int,
     device: torch.device,
-    sampling_timesteps: Optional[int]) -> ConditionalGaussianDiffusion:
+    sampling_timesteps: Optional[int] = None) -> ConditionalGaussianDiffusion:
 
     timesteps           = train_ddpm_cfg['timesteps']
     sampling_timesteps  = train_ddpm_cfg['sampling_timesteps'] \
@@ -99,6 +99,7 @@ def load_cddpm_from_configs_and_cpt(
     ema_update_every = 10
     ema_decay = 0.995
     ddpm_ema = EMA(ddpm, beta = ema_decay, update_every = ema_update_every)
+    ddpm_ema = ddpm_ema.to(device)
     ddpm_ema.load_state_dict(cpt['ema'])
     
     return ddpm_ema.model

tta_uia_segmentation/src/tta/TTADAEandDDPM.py
@@ -58,9 +58,9 @@ class TTADAEandDDPM(TTADAE):
         max_t_diffusion_tta: int = 999,
         sampling_timesteps: Optional[int] = None,
         min_max_int_norm_imgs: tuple[float, float] = (0, 1),
-        use_x_norm_for_ddpm_loss: bool = True,
+        detach_x_norm_from_ddpm_loss: bool = True,
         use_y_pred_for_ddpm_loss: bool = False,
-        use_x_cond_gt: bool = False,    # Of course use only for debugging
+        use_y_gt_for_ddpm_loss: bool = False,    # Of course use only for debugging
         use_ddpm_after_step: Optional[int] = None,
         use_ddpm_after_dice: Optional[float] = None,
         warmup_steps_for_ddpm_loss: Optional[int] = None,
@@ -90,7 +90,7 @@ class TTADAEandDDPM(TTADAE):
         self.max_int_norm_imgs = min_max_int_norm_imgs[1]
         
         self.use_y_pred_for_ddpm_loss = use_y_pred_for_ddpm_loss   
-        self.use_x_norm_for_ddpm_loss = use_x_norm_for_ddpm_loss 
+        self.detach_x_norm_from_ddpm_loss = detach_x_norm_from_ddpm_loss 
         
         self.sampling_timesteps = sampling_timesteps
         self.ddpm.set_sampling_timesteps(sampling_timesteps)
@@ -100,7 +100,7 @@ class TTADAEandDDPM(TTADAE):
         self.ddpm.requires_grad_(False)
         
         # Attributes used only for debugging
-        self.use_x_cond_gt = use_x_cond_gt
+        self.use_y_gt_for_ddpm_loss = use_y_gt_for_ddpm_loss
         
         # Setup the custom metrics and steps wandb
         if self.wandb_log:
@@ -391,12 +391,12 @@ class TTADAEandDDPM(TTADAE):
                     del x_norm
                     
                     # Fix x_cond depending on the configuration
-                    if self.use_x_cond_gt:
+                    if self.use_y_gt_for_ddpm_loss:
                         # Only for debugging
-                        x_cond = y_gt.to(device)
+                        x_cond = y_gt.type(torch.int8).to(device).typ
                         
                     elif not self.use_y_pred_for_ddpm_loss:
-                        x_cond = y_pl.to(device)
+                        x_cond = y_pl.type(torch.int8).to(device)
                         
                         # Upsample the segmentation mask to the same size as the input image, if neccessary
                         rescale_factor = np.array(x.shape) / np.array(x_cond.shape)
@@ -549,17 +549,22 @@ class TTADAEandDDPM(TTADAE):
                 x_cond_mb = x_cond[i: i + minibatch_size]
                     
             # Normalize the input image between 0 and 1, (required by the DDPM)
-            x_norm_mb = du.normalize_min_max(
-                x_norm_mb,
+            x_norm_mb_ddpm = x_norm_mb if not self.detach_x_norm_from_ddpm_loss else x_norm_mb.detach().clone()
+            
+            x_norm_mb_ddpm = du.normalize_min_max(
+                x_norm_mb_ddpm,
                 min=min_int_norm_imgs, 
                 max=max_int_norm_imgs
                 )
             
-            if x_norm_mb.max() > 1 + min_max_tolerance or x_norm_mb.min() < 0 - min_max_tolerance:
-                print(f'WARNING: x_norm_mb.max()={x_norm_mb.max()}, x_norm_mb.min()={x_norm_mb.min()}')
+            if x_norm_mb_ddpm.max() > 1 + min_max_tolerance or x_norm_mb_ddpm.min() < 0 - min_max_tolerance:
+                print(f'WARNING: x_norm_mb.max()={x_norm_mb_ddpm.max()}, x_norm_mb.min()={x_norm_mb.min()}')
             
             # Calculate the DDPM loss and backpropagate
-            ddpm_loss = ddpm_reweigh_factor * self.ddpm(x_norm_mb, x_cond_mb)
+            ddpm_loss = ddpm_reweigh_factor * self.ddpm(
+                x_norm_mb_ddpm, 
+                x_cond_mb
+                )
             ddpm_loss.backward()
             
             ddpm_loss_value += ddpm_loss.detach()