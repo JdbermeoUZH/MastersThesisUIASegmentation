cfbbf57084dbc52b7a04462052b03b1c19b51e38
Juan-Diego Bermeo-Ortiz <jbermeo@biwidl319.ee.ethz.ch>
2024-06-24 13:39:44 +0200

    added DiffTTA

--- scripts/tta/diffusion_tta_wmh_w_synthseg_labels_a6000.sh
+++ scripts/tta/diffusion_tta_wmh_w_synthseg_labels_a6000.sh
@@ -4,11 +4,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --constraint='a6000'
 
+
 source /scratch_net/biwidl319/jbermeo/conda/conda/etc/profile.d/conda.sh
 conda activate /scratch_net/biwidl319/jbermeo/GNN-Domain-Generalization-main/net_scratch/conda_envs/tta_uia_seg
 
 python diffusion_tta.py \
  /scratch_net/biwidl319/jbermeo/MastersThesisUIASegmentation/config/datasets.yaml \
- /scratch_net/biwidl319/jbermeo/MastersThesisUIASegmentation/config/tta/tta_brain_hcp_t1w.yaml \
+ /scratch_net/biwidl319/jbermeo/MastersThesisUIASegmentation/config/tta/tta_wmh_umc_w_synthseg_labels.yaml \
  "$@"  
-#  --start 0 \
+
